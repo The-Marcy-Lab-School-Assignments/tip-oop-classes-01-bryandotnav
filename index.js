eaec4cb6934f0a07906e0eb43f990b27d3c0ca65
Bryan <bryan001700@gmail.com>
2025-11-17 10:00:55 -0500

    completed some of the tip challenge

--- index.js
+++ index.js
@@ -6,7 +6,25 @@
 
 class Task {
  //write your code here
- 
+ #completed;
+ #minutesSpent;
+ constructor(title, priority){
+    this.title = title;
+    this.priority = priority;
+    this.#completed = false;
+    this.minutesSpent = 0;
+ }
+ workOn(minutes){
+    this.minutesSpent += minutes;
+    return `Worked on ${this.title} for ${this.minutes} minutes. Total time: ${this.minutesSpent} minutes`
+ }
+ complete(){
+    this.#completed = true;
+    return `${this.title} has been completed!`
+ }
+ iscomplete(){
+    return this.#completed;
+ }
 }
 
 //Test class with examples below:
